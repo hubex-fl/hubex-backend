3c240c628a793af50fb7912009fbf888e295a360
hubex-fl <langenflorian@gmail.com>
2025-12-20 07:25:32 +0100

    chore(frontend): add vue module typing shim for tsc

---
+++ frontend/src/env.d.ts
@@ -0,0 +1,6 @@
+﻿/// <reference types="vite/client" />
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
